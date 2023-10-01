fd355be839f6c5d8b42de3c59329f75e991983d0
Nyttho <anthony.dms@outlook.com>
2023-10-01 13:11:27 +0200

    suppression fichiers inutiles + correction retour accueil page 404

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
     <div className="error-page">
       <h2>404</h2>
       <p>Oups ! La page que vous demandez n'existe pas.</p>
-      <a href="/">Retourner sur la page d'accueil</a>
+      <Link to="/">Retourner sur la page d'accueil</Link>
     </div>
   );
 };
